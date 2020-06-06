63491de68a15002a6c86ed45159751688920cb3d
Mashrur Chowdhury <mashrurc02@gmail.com>
2020-06-06 11:28:53 -0400

    tips

--- lib/tips.dart
+++ lib/tips.dart
@@ -15,32 +15,36 @@ class _TipsState extends State<Tips> {
         margin: EdgeInsets.symmetric(vertical: 0.0),
         height: 800.0,
         child: ListView(
-          scrollDirection: Axis.horizontal,
+          scrollDirection: Axis.vertical,
           children: <Widget>[
             Container(
               child: Text("Tip#1 drink water pls"),
               width: 400.0,
-              height: 800.0,
+              height: 50.0,
               color: Colors.blue[100],
             ),
             Container(
               child: Text("Tip#1 drink water pls"),
               width: 400.0,
+              height: 50.0,
               color: Colors.blue,
             ),
             Container(
               child: Text("Tip#1 drink water pls"),
               width: 400.0,
+              height: 50.0,
               color: Colors.green,
             ),
             Container(
               child: Text("Tip#1 drink water pls"),
               width: 400.0,
+              height: 50.0,
               color: Colors.yellow,
             ),
             Container(
               child: Text("Tip#1 drink water pls"),
               width: 400.0,
+              height: 50.0,
               color: Colors.orange,
             ),
           ],
