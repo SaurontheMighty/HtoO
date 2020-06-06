cfd81b8739a7fca43e621aab4f1752850363b5f6
Ashish <yesashishs@gmail.com>
2020-06-06 11:22:59 -0400

    Map with Dark Theme

--- lib/maps.dart
+++ lib/maps.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class Maps extends StatefulWidget {
   @override
@@ -7,10 +8,41 @@ class Maps extends StatefulWidget {
 }
 
 class _MapsState extends State<Maps> {
+  GoogleMapController mapController;
+  String _mapStyle;
+
+  final LatLng _center = const LatLng(43.588083, -79.642514);
+
+  @override
+  void initState() {
+    super.initState();
+    rootBundle.loadString('assets/mapstyle.txt').then((string) {
+      _mapStyle = string;
+    });
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+    mapController.setMapStyle(_mapStyle);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 14.0,
+              ),
+              myLocationEnabled: true,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
