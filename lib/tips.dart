cb16c58a0316af72b0ad38021e395ea2577211dc
miphc42 <4hmin2@gmail.com>
2020-06-06 17:22:48 -0400

    fixxx

--- lib/tips.dart
+++ lib/tips.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+final dbRef = Firestore.instance;
 class Tips extends StatefulWidget {
   @override
   _TipsState createState() => _TipsState();
